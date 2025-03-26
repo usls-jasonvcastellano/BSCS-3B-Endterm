4acd8dcc55159a9175388cc8f43fcc3bdb8a16a6
CHAJZ G. BOLETIN <2010550@cc.usls.edu>
2025-03-26 09:07:03 +0800

    First commit: Done moving all of my project files (Incomplete Project)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route:: get('/item', 'App\Http\Controllers\ItemController@index');
+Route:: get('/login', 'App\Http\Controllers\LoginController@index');
+
+Route:: get('/aboutme', 'App\Http\Controllers\AboutmeController@index');
+
+Route:: get('/hello', 'App\Http\Controllers\HelloController@index');
+Route:: get('/inventory', 'App\Http\Controllers\InventoryController@index');
